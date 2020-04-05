dce121c7b94a285cbb6053e42b8a840fcd9c84fc
goodListener <good_listener@naver.com>
2020-04-05 23:59:22 +0900

    로그인 분리

---
+++ src/component/login/Login.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import fireStore from '../../fireStore/fireStore';
+import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { login, logout } from '../../store/user';
+
+class Login extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            userName: ''
+        }
+    }
+
+    login = (event) => {
+        event.preventDefault();
+        var userData = fireStore.collection('users').doc(this.state.userName);
+        userData.get().then(doc => {
+            if (doc.exists) {
+                this.props.login(doc.data().userId);
+            } else {
+                console.log('not exist user');
+            }
+        }).catch(error => {
+            console.error(error);
+        })
+    }
+
+    logout = (event) => {
+        this.props.logout();
+    }
+
+    handleNameChange = (event) => {
+        this.setState({
+            userName: event.target.value
+        })
+    }
+
+    render () {
+        return ((() => {
+            if (this.props.user.isLogined) {
+                return (
+                <Button type="button" onClick={this.logout}>LOGOUT</Button>
+            )} else {
+                return (
+                    <form onSubmit={this.login}>
+                        <TextField 
+                        id="userName"
+                        name="userName"
+                        onChange={this.handleNameChange}
+                        label="이름"></TextField>
+                        <Button type="submit">LOGIN</Button>
+                    </form>
+                )
+            }
+        })())
+    }
+}
+
+export default connect(
+    state => ({ user: state }),
+    { login, logout }
+) (Login);
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,55 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import fireStore from '../../fireStore/fireStore';
 import { connect } from 'react-redux';
-import { login, logout } from '../../store/user';
+import Login from '../../component/login/Login'
 
 class Home extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            userName: ''
-        }
-    }
-
-    login = (event) => {
-        event.preventDefault();
-        var userData = fireStore.collection('users').doc(this.state.userName);
-        userData.get().then(doc => {
-            if (doc.exists) {
-                this.props.login(doc.data().userId);
-            } else {
-                console.log('not exist user');
-            }
-        }).catch(error => {
-            console.error(error);
-        })
-    }
-
-    logout = (event) => {
-        this.props.logout();
-    }
-
-    handleNameChange = (event) => {
-        this.setState({
-            userName: event.target.value
-        })
-    }
-
     render () {
         return (
             <div className="home">
-                <form onSubmit={this.login}>
-                    <TextField 
-                    id="userName"
-                    name="userName"
-                    onChange={this.handleNameChange}
-                    label="이름"></TextField>
-                    <Button type="submit">LOGIN</Button>
-                </form>
-                <Button type="button" onClick={this.logout}>LOGOUT</Button>
+                <Login></Login>
                 <div><Link to="work">출퇴근</Link></div>
                 <div><Link to="study">공부 타이머</Link></div>
                 <div><Link to="logs">기록확인</Link></div>
@@ -60,5 +18,5 @@ class Home extends React.Component {
 
 export default connect(
     state => ({ user: state }),
-    { login, logout }
+    { }
 ) (Home);
